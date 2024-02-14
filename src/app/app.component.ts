24362a179fabe352bda69fa18f452ef0f6ae1ced
Christian Rincon <christianrinconalem@gmail.com>
2024-02-14 01:03:34 -0300

    feat: Agregado del Balance-Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatAnchor } from '@angular/material/button';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
-import { ShyftApiService } from './shyft-api.service';
-import { WalletStore } from '@heavy-duty/wallet-adapter';
-import { computedAsync } from 'ngxtension/computed-async';
-import { toSignal } from '@angular/core/rxjs-interop';
 
 
 @Component({
   standalone: true,
-  imports: [HdWalletMultiButtonComponent, RouterModule],
+  imports: [HdWalletMultiButtonComponent, RouterModule, MatAnchor],
   selector: 'bob-root',
   template:`
     <header class="px-16 pt-20 pb-8 relative">
       <h1 class= "text-center text-5xl mb-4">My bank</h1>
-
       <div class="flex justify-center mb-4">
-        <hd-wallet-multi-button></hd-wallet-multi-button>
-      </div>
-      @if(account()){
-        <div class="absolute top-4 left-4 flex items-center gap-2">
-          <img [src]="account()?.info?.image" class="w-8 h-8"/>
-          <p class="text-2xl font-bold">
-            {{ account()?.info?.name }}
-          </p>
-          <p class="text-2xl font-bold">
-            {{ account()?.balance  }}
-          </p>
-        </div>
-      }
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+    </div>
+      <nav>
+        <ul class="flex justify-center items-center gap-4">
+          <li>
+            <a [routerLink] = "['balance']" mat-raised-button> Balance </a>
+          <li>
+          <li>
+            <a [routerLink] = "['settings']" mat-raised-button> Settings </a>
+          <li>
+        </ul>
+      </nav>
     </header>
     
     <main>
@@ -37,12 +32,5 @@ import { toSignal } from '@angular/core/rxjs-interop';
   `,
 })
 export class AppComponent {
-  private readonly _shiftyApiService = inject(ShyftApiService);
-  private readonly _walletStore = inject(WalletStore);
-  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
-
-  readonly account = computedAsync(
-    () => this._shiftyApiService.getAccount(this._publicKey()?.toBase58()),
-  { requireSync:true },
-  ); 
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path:'settings',
+        loadComponent: () =>
+        import('./settings.component').then((data) => data.SettingsComponent)
+    },
+    {
+        path:'balance',
+        loadComponent: () => 
+        import('./balance-page.component').then((data) => data.BalancePageComponent)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
