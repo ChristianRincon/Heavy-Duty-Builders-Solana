08d60fb5b63f7bb6b11e75967c8fd2540513eedf
Christian Rincon <christianrinconalem@gmail.com>
2024-02-14 01:12:34 -0300

    feat: Agregado del Balance-Section

---
+++ src/app/settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component(
+    {
+        selector: 'settings-selector',
+        standalone: true,
+        template: `<p>Settings</p>`
+    }
+)
+
+export class SettingsComponent{}
