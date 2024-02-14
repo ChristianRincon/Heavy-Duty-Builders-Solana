7d6b0392d87d45ccd66790fb215a8fd67ce0d221
Christian Rincon <christianrinconalem@gmail.com>
2024-02-13 22:35:08 -0300

    feat: Ver Balance de Token
    - Consumimos servicio de Shyft para ver balance de un token específico.
    
    - Se debe cambiar el mint para ver el balance de cada token.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './shyft-api.service';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { computedAsync } from 'ngxtension/computed-async';
+import { toSignal } from '@angular/core/rxjs-interop';
+
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'solana-bootcamp-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [HdWalletMultiButtonComponent, RouterModule],
+  selector: 'bob-root',
+  template:`
+    <header class="px-16 pt-20 pb-8 relative">
+      <h1 class= "text-center text-5xl mb-4">My bank</h1>
+
+      <div class="flex justify-center mb-4">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
+      @if(account()){
+        <div class="absolute top-4 left-4 flex items-center gap-2">
+          <img [src]="account()?.info?.image" class="w-8 h-8"/>
+          <p class="text-2xl font-bold">
+            {{ account()?.info?.name }}
+          </p>
+          <p class="text-2xl font-bold">
+            {{ account()?.balance  }}
+          </p>
+        </div>
+      }
+    </header>
+    
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
 })
 export class AppComponent {
-  title = 'solana-bootcamp';
+  private readonly _shiftyApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  readonly account = computedAsync(
+    () => this._shiftyApiService.getAccount(this._publicKey()?.toBase58()),
+  { requireSync:true },
+  ); 
 }
---
+++ src/app/shyft-api.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { map, of } from "rxjs";
+import { environment } from "src/environment/environment";
+
+@Injectable({ providedIn: 'root' })
+
+export class ShyftApiService {
+
+    private readonly _httpClient = inject(HttpClient);
+    private readonly _header = { 'x-api-key': environment.API_TOKEN };
+    private readonly _mint = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+
+    getAccount(publicKey: string | undefined | null) {
+        if (!publicKey) {
+            return of(null);
+        }
+        
+        const url = new URL('https://api.shyft.to/sol/v1/wallet/token_balance');
+        url.searchParams.append('network', 'mainnet-beta');
+        url.searchParams.append('wallet', publicKey);
+        url.searchParams.append('token', this._mint);
+
+        return this._httpClient.get<{ result: { balance: number; info: { name: string, image: string } } }>(
+            url.toString(),
+            { headers: this._header }
+        )
+            .pipe(map(({ result }) => result));
+    }
+
+}
