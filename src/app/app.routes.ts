285e3d0a7f718810cced3a19e3badf80e080f77a
Christian Rincon <christianrinconalem@gmail.com>
2024-02-14 01:07:25 -0300

    Revert "feat: Agregado del Balance-Section"
    This reverts commit 24362a179fabe352bda69fa18f452ef0f6ae1ced.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,3 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [
-    {
-        path:'settings',
-        loadComponent: () =>
-        import('./settings.component').then((data) => data.SettingsComponent)
-    },
-    {
-        path:'balance',
-        loadComponent: () => 
-        import('./balance-page.component').then((data) => data.BalancePageComponent)
-    },
-    {
-        path: '**',
-        redirectTo: ''
-    }
-];
+export const appRoutes: Route[] = [];
