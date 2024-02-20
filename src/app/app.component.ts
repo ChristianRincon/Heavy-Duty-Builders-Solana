f3053ff89e2d833aeedf1f3ce1dae3585d70f06f
Christian Rincon <christianrinconalem@gmail.com>
2024-02-20 01:41:23 -0300

    feat: Agregado de Modal y Formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatAnchor } from '@angular/material/button';
 import { HdWalletMultiButtonComponent } from "@heavy-duty/wallet-adapter-material";
+import { MatDialog } from '@angular/material/dialog';
+import { TransferModalComponent } from './transfer-modal.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   standalone: true,
-  imports: [HdWalletMultiButtonComponent, RouterModule, MatAnchor],
+  imports: [HdWalletMultiButtonComponent, RouterModule, MatAnchor, MatButtonModule, MatIcon],
   selector: 'solana-bootcamp-root',
   template:`
   <header class="py-8 relative">
-    <h1 class="text-5xl text-center mb-4">Banco de Christian</h1>
 
-    <div class="flex justify-center mt-5 mb-6">
+    <div class="grid justify-items-end mr-10">
       <hd-wallet-multi-button></hd-wallet-multi-button>
     </div>
 
-    <nav>
-      <ul class="flex justify-center items-center gap-4">
+    <h1 class="text-5xl text-center mb-10">Banco de Christian</h1>
+
+    <nav class="mb-8">
+      <ul class="flex justify-center items-center gap-7">
         <li>
-          <a [routerLink]="['balance']" mat-raised-button>Balance</a>
+          <button (click)="onTransfer()" mat-raised-button color="primary">
+            <mat-icon matSuffix>ios_share</mat-icon>
+            Transferir
+          </button>
         </li>
         <li>
-          <a [routerLink]="['transaction']" mat-raised-button>Historial</a>
+          <a [routerLink]="['balance']" mat-raised-button>
+            <mat-icon matSuffix>account_balance</mat-icon>
+              Ver Balance
+          </a>
+        </li>
+        <li>
+          <a [routerLink]="['transaction']" mat-raised-button>
+            <mat-icon matSuffix>history</mat-icon>
+              Ver Historial
+          </a>
         </li>
       </ul>
     </nav>
+
   </header>
 
   <main>
@@ -33,5 +51,9 @@ import { HdWalletMultiButtonComponent } from "@heavy-duty/wallet-adapter-materia
   `
 })
 export class AppComponent {
+  private readonly _matdialog = inject(MatDialog);
   
+  onTransfer(){
+    this._matdialog.open(TransferModalComponent);
+  }
 }
